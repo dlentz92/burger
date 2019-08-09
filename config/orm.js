649de06b945866fc391d72db9cdf2b5e7eb9ca0b
Danit Lentz <danitlentz@Danits-MacBook-Pro.local>
2019-08-08 21:09:13 -0400

    added selectall()

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,26 @@
 var connection = require("../config/connection.js");
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+function objToSql(ob) {selectAll
+    var arr = [];
+    for (var key in ob) {
+      var value = ob[key];
+      if (Object.hasOwnProperty.call(ob, key)) {
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+      }
+    }
+    return arr.toString();
+  }
+
+
+module.exports = orm;
