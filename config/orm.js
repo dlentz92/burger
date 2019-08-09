babb296fd1b264eae24f1666d0cce959003b0c83
Danit Lentz <danitlentz@Danits-MacBook-Pro.local>
2019-08-08 21:14:18 -0400

    orm set up

--- config/orm.js
+++ config/orm.js
@@ -8,19 +8,53 @@ function printQuestionMarks(num) {
 
     return arr.toString();
 }
-function objToSql(ob) {selectAll
+function objToSql(ob) {
+    selectAll
     var arr = [];
     for (var key in ob) {
-      var value = ob[key];
-      if (Object.hasOwnProperty.call(ob, key)) {
-        if (typeof value === "string" && value.indexOf(" ") >= 0) {
-          value = "'" + value + "'";
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
         }
-        arr.push(key + "=" + value);
-      }
     }
     return arr.toString();
-  }
+}
+var orm = {
+    one: function (table, condition, cb) {
+        var queryString = "SELECT * FROM " + table;
 
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    update: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+}
 
 module.exports = orm;
