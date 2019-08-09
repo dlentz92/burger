f131c5c073e1cdc78e48163fb574a4e8cebff158
Danit Lentz <danitlentz@Danits-MacBook-Pro.local>
2019-08-09 00:30:16 -0400

    updated orm. connection working

--- config/orm.js
+++ config/orm.js
@@ -1,15 +1,15 @@
 var connection = require("../config/connection.js");
-// function printQuestionMarks(num) {
-//     var arr = [];
+function printQuestionMarks(num) {
+    var arr = [];
 
-//     for (var i = 0; i < num; i++) {
-//         arr.push("?");
-//     }
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
 
-//     return arr.toString();
-// }
+    return arr.toString();
+}
 function objToSql(ob) {
-    selectAll
+
     var arr = [];
     for (var key in ob) {
         var value = ob[key];
@@ -38,16 +38,28 @@ var orm = {
             cb(result);
         });
     },
-    update: function (table, objColVals, condition, cb) {
-        var queryString = "UPDATE " + table;
+    all: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    create: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
 
-        queryString += " SET ";
-        queryString += objToSql(objColVals);
-        queryString += " WHERE ";
-        queryString += condition;
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
 
         console.log(queryString);
-        connection.query(queryString, function (err, result) {
+
+        connection.query(queryString, vals, function (err, result) {
             if (err) {
                 throw err;
             }
